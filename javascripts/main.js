8ce2fa91b47e961d1bd038b77a242844f43010a1
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 11:13:30 -0600

    Flipped code around to satisfy readme.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,11 @@
 "use strict";
 
-const earth = require("./earth");
+
 const view = require("./view");
-const _capitalize = require("lodash.capitalize");
-const Handlebars = require("handlebars");
 
-Handlebars.registerHelper('capitalize', function(item) {
-    return _capitalize(item);
-});
-
+const earth = require("./earth");
 view.outputToDom(earth.getPlanetInfo());
+
+
 
 
