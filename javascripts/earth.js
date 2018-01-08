15b5452049e6cebcd080f8f81ce0b66af056ca3d
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 10:13:07 -0600

    Changed underscore to camal case.

--- javascripts/earth.js
+++ javascripts/earth.js
@@ -5,18 +5,18 @@ let planetInformation = {
     "yearDiscovered" : "NA",
     "mass": 7024597220000000000, // KG
     "size": 3959, // Mi
-    "distance_from_sun": {
+    "distanceFromSun": {
         "aphelion": 94500000, //mi
         "perihelion": 91401000 //mi
     },
-    "atmosphere_composition":{
+    "atmosphereComposition":{
         "nitrogen": 78.08,
         "oxygen": 20.95,
         "argon": 0.930,
         "carbon dioxide": 0.0402
     },
     "satellite": "Just a few.",
-    "probes_and_explorers": "All of humanity."
+    "probesAndExplorers": "All of humanity."
 };
 
 module.exports.getPlanetInfo = () => planetInformation;
