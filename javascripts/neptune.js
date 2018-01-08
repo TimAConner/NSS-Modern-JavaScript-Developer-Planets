78ae615bd166ef4c3b805b1bb0bce3d10cbfc907
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 11:37:02 -0600

    Add neptune dummy data.

---
+++ javascripts/neptune.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let planetInformation = {
+    "name": "Neptune",
+    "yearDiscovered" : "NA",
+    "mass": 7024597220000000000, // KG
+    "size": 3959, // Mi
+    "distanceFromSun": {
+        "aphelion": 94500000, //mi
+        "perihelion": 91401000 //mi
+    },
+    "atmosphereComposition":{
+        "nitrogen": 78.08,
+        "oxygen": 20.95,
+        "argon": 0.930,
+        "carbon dioxide": 0.0402
+    },
+    "satellite": "Just a few.",
+    "probesAndExplorers": "All of humanity."
+};
+
+module.exports.getPlanetInfo = () => planetInformation;
