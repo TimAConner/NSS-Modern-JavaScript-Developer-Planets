07df77fee4d7e7286e8374346fb7d724c281a643
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 09:44:35 -0600

    Add earth planet information.

---
+++ javascripts/earth.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let planetInformation = {
+    "name": "Earth",
+    "yearDiscovered" : "NA",
+    "mass": 7024597220000000000, // KG
+    "size": 3959, // Mi
+    "distance_from_sun": {
+        "aphelion": 94500000, //mi
+        "perihelion": 91401000 //mi
+    },
+    "atmosphere_composition":{
+        "nitrogen": 78.08,
+        "oxygen": 20.95,
+        "argon": 0.930,
+        "carbon dioxide": 0.0402
+    },
+    "satellite": "Just a few.",
+    "probes_and_explorers": "All of humanity."
+};
+
+module.exports.outputTo = () => {
+
+};
