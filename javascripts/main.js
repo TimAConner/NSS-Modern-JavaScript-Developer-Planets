ad88e37ddb5a3f476154fa0776293b32c2ee64d7
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 11:02:37 -0600

    Add capitalize helper to handlebars.

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,11 @@
 const earth = require("./earth");
 const view = require("./view");
 const _capitalize = require("lodash.capitalize");
+const Handlebars = require("handlebars");
+
+Handlebars.registerHelper('capitalize', function(item) {
+    return _capitalize(item);
+});
 
 view.outputToDom(earth.getPlanetInfo());
 
