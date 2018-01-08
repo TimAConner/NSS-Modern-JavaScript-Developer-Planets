a3f44da1b4208bb8afb55afe265633eba326ddf6
Tim A. Conner <timaconner1@gmail.com>
2018-01-08 11:41:12 -0600

    Output each planet information.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,35 @@
 
 
 const view = require("./view");
+const helper = require("./helper");
+
 
 const earth = require("./earth");
 view.outputToDom(earth.getPlanetInfo());
 
+const jupiter = require("./jupiter");
+view.outputToDom(jupiter.getPlanetInfo());
 
 
+const mars = require("./mars");
+view.outputToDom(mars.getPlanetInfo());
 
+
+const mercury = require("./mercury");
+view.outputToDom(mercury.getPlanetInfo());
+
+
+const neptune = require("./neptune");
+view.outputToDom(neptune.getPlanetInfo());
+
+
+const saturn = require("./saturn");
+view.outputToDom(saturn.getPlanetInfo());
+
+
+const uranus = require("./uranus");
+view.outputToDom(uranus.getPlanetInfo());
+
+
+const venus = require("./venus");
+view.outputToDom(venus.getPlanetInfo());
